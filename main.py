321e56b5b4e52d70a11c19846bd110872af8eefd
njahn88 <nja010@email.latech.edu>
2023-12-02 16:55:34 -0600

    Added variables for the colors of the buttons

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from typing import Optional, Tuple, Union
 import customtkinter
 
 class Calulator(customtkinter.CTk):
+
     def __init__(self):
         super().__init__()
         customtkinter.set_appearance_mode('dark')
         customtkinter.set_default_color_theme("dark-blue")
+
+        lightGrayFgColor = "#b0b0b0"
+        lightGrayHoverColor = "#e0e0de"
+        darkGrayFgColor = "#292929"
+        darkGrayHoverColor = "#6b6b6b"
+        orangeFgColor = "#c77e10"
+        orangeHoverColor = "#c48839"
 
         self.title("Calculator")
         self.geometry("320x350")
@@ -20,65 +28,65 @@ class Calulator(customtkinter.CTk):
         entry.grid(row=0, column=0, columnspan=4, pady=(35, 3))
 
         #row 1
-        button1 = customtkinter.CTkButton(master=frame, width=40, height=40, text="AC", fg_color="#b0b0b0", text_color="black", hover_color="#e0e0de")
+        button1 = customtkinter.CTkButton(master=frame, width=40, height=40, text="AC", fg_color=lightGrayFgColor, text_color="black", hover_color=lightGrayHoverColor)
         button1.grid(row=1, column=0, padx=5, pady=3)
 
-        button2 = customtkinter.CTkButton(master=frame, width=40, height=40, text="+/-", fg_color="#b0b0b0", text_color="black", hover_color="#e0e0de")
+        button2 = customtkinter.CTkButton(master=frame, width=40, height=40, text="+/-", fg_color=lightGrayFgColor, text_color="black", hover_color=lightGrayHoverColor)
         button2.grid(row=1, column=1, padx=5, pady=3)
 
-        button3 = customtkinter.CTkButton(master=frame, width=40, height=40, text="%", fg_color="#b0b0b0", text_color="black", hover_color="#e0e0de")
+        button3 = customtkinter.CTkButton(master=frame, width=40, height=40, text="%", fg_color=lightGrayFgColor, text_color="black", hover_color=lightGrayHoverColor)
         button3.grid(row=1, column=2, padx=5, pady=3)
 
-        button4 = customtkinter.CTkButton(master=frame, width=40, height=40, text="/", fg_color="#c77e10", text_color="white", hover_color="#c48839")
+        button4 = customtkinter.CTkButton(master=frame, width=40, height=40, text="/", fg_color=orangeFgColor, text_color="white", hover_color=orangeHoverColor)
         button4.grid(row=1, column=3, padx=5, pady=3)
 
         #row2
-        button1b = customtkinter.CTkButton(master=frame, width=40, height=40, text="7", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button1b = customtkinter.CTkButton(master=frame, width=40, height=40, text="7", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button1b.grid(row=2, column=0, padx=5, pady=3)
 
-        button2b = customtkinter.CTkButton(master=frame, width=40, height=40, text="8", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button2b = customtkinter.CTkButton(master=frame, width=40, height=40, text="8", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button2b.grid(row=2, column=1, padx=5, pady=3)
 
-        button3b = customtkinter.CTkButton(master=frame, width=40, height=40, text="9", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button3b = customtkinter.CTkButton(master=frame, width=40, height=40, text="9", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button3b.grid(row=2, column=2, padx=5, pady=3)
 
-        button4b = customtkinter.CTkButton(master=frame, width=40, height=40, text="X", fg_color="#c77e10", text_color="white", hover_color="#c48839")
+        button4b = customtkinter.CTkButton(master=frame, width=40, height=40, text="X", fg_color=orangeFgColor, text_color="white", hover_color=orangeHoverColor)
         button4b.grid(row=2, column=3, padx=5, pady=3)
 
         #row3
-        button1c = customtkinter.CTkButton(master=frame, width=40, height=40, text="4", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button1c = customtkinter.CTkButton(master=frame, width=40, height=40, text="4", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button1c.grid(row=3, column=0, padx=5, pady=3)
 
-        button2c = customtkinter.CTkButton(master=frame, width=40, height=40, text="5", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button2c = customtkinter.CTkButton(master=frame, width=40, height=40, text="5", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button2c.grid(row=3, column=1, padx=5, pady=3)
 
-        button3c = customtkinter.CTkButton(master=frame, width=40, height=40, text="6", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button3c = customtkinter.CTkButton(master=frame, width=40, height=40, text="6", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button3c.grid(row=3, column=2, padx=5, pady=3)
 
-        button4c = customtkinter.CTkButton(master=frame, width=40, height=40, text="-", fg_color="#c77e10", text_color="white", hover_color="#c48839")
+        button4c = customtkinter.CTkButton(master=frame, width=40, height=40, text="-", fg_color=orangeFgColor, text_color="white", hover_color=orangeHoverColor)
         button4c.grid(row=3, column=3, padx=5, pady=3)
 
         #row4
-        button1c = customtkinter.CTkButton(master=frame, width=40, height=40, text="1", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button1c = customtkinter.CTkButton(master=frame, width=40, height=40, text="1", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button1c.grid(row=4, column=0, padx=5, pady=3)
 
-        button2c = customtkinter.CTkButton(master=frame, width=40, height=40, text="2", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button2c = customtkinter.CTkButton(master=frame, width=40, height=40, text="2", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button2c.grid(row=4, column=1, padx=5, pady=3)
 
-        button3c = customtkinter.CTkButton(master=frame, width=40, height=40, text="3", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button3c = customtkinter.CTkButton(master=frame, width=40, height=40, text="3", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button3c.grid(row=4, column=2, padx=5, pady=3)
 
-        button4c = customtkinter.CTkButton(master=frame, width=40, height=40, text="+", fg_color="#c77e10", text_color="white", hover_color="#c48839")
+        button4c = customtkinter.CTkButton(master=frame, width=40, height=40, text="+", fg_color=orangeFgColor, text_color="white", hover_color=orangeHoverColor)
         button4c.grid(row=4, column=3, padx=5, pady=3)
 
         #row4
-        button1d = customtkinter.CTkButton(master=frame, width=90, height=40, text="0", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button1d = customtkinter.CTkButton(master=frame, width=90, height=40, text="0", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button1d.grid(row=5, column=0, columnspan=2, padx=5, pady=3)
 
-        button2d = customtkinter.CTkButton(master=frame, width=40, height=40, text=".", fg_color="#292929", text_color="white", hover_color="#6b6b6b")
+        button2d = customtkinter.CTkButton(master=frame, width=40, height=40, text=".", fg_color=darkGrayFgColor, text_color="white", hover_color=darkGrayHoverColor)
         button2d.grid(row=5, column=2, padx=5, pady=3)
 
-        button3d = customtkinter.CTkButton(master=frame, width=40, height=40, text="=", fg_color="#c77e10", text_color="white", hover_color="#c48839")
+        button3d = customtkinter.CTkButton(master=frame, width=40, height=40, text="=", fg_color=orangeFgColor, text_color="white", hover_color=orangeHoverColor)
         button3d.grid(row=5, column=3, padx=5, pady=3)
 
 
